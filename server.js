02813e84f40fbc9ffce537cee11e08d0cc7afeed
lasb72 <lasb72@hotmail.com>
2022-04-03 11:29:25 -0500

    mi primer commit

--- server.js
+++ server.js
@@ -1,28 +1,22 @@
-/*const path = require('path');
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
+
 const express = require('express');
 const app = express();
 
-// Serve static files
-app.use(express.static(__dirname + '/dist/xxx'));
+app.use(requireHTTPS);
+app.use(express.static('./dist/xxx'));    
 
-// Send all requests to index.html
-app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/xxx'));
-});
+app.get('/*', (req, res) =>
+  res.sendFile('index.html', {root: 'dist/xxx/'}),
+);
 
-// default Heroku port
-app.listen(process.env.PORT || 5000);*/
+app.listen(process.env.PORT || 8080);
 
 
 
-
-const jsonServer = require("json-server");
-const server = jsonServer.create();
-const router = jsonServer.router("src/db/db.json");
-const middlewares = jsonServer.defaults({ static: "./build" });
-const port = process.env.PORT || 3000;
-
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
